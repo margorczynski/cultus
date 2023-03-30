df65aa7266107d62c87dbeac268c5a22ba588230
Marcin Gorczyński <margorczynski@gmail.com>
2023-03-30 02:50:23 +0200

    Implement tests for next_state. Fix logic bugs

--- src/game/game_state.rs
+++ src/game/game_state.rs
@@ -1,8 +1,10 @@
+use log::debug;
 use crate::game::game_object::GameObject;
 use crate::game::game_state::GameState::{Finished, InProgress};
 use crate::game::game_action::GameAction;
 use super::level::Level;
 
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub enum GameState {
     InProgress(Level, usize, usize),
     Finished(usize)
@@ -17,6 +19,8 @@ impl GameState {
     pub fn next_state(&self, game_action: GameAction) -> GameState {
         match self {
             InProgress(current_level, current_step, current_points) => {
+                debug!("next_state for current InProgress - current_step: {}, current_points: {}", *current_step, *current_points);
+
                 if *current_step >= current_level.max_steps {
                     return Finished(*current_points)
                 }
@@ -38,14 +42,75 @@ impl GameState {
                     }
                 };
 
-                if current_level.get_point_amount() == 0 {
-                    let bonus_points = current_level.max_steps - current_step;
+                let new_current_step = current_step + 1;
+
+                debug!("Level points left: {}, new_points: {}", current_level.get_point_amount(), new_points);
+
+                if updated_level.get_point_amount() == 0 {
+                    let bonus_points = current_level.max_steps - new_current_step;
+                    debug!("Game won - bonus_points: {}", bonus_points);
                     Finished(new_points + bonus_points)
                 } else {
-                    InProgress(updated_level, current_step + 1, new_points )
+                    InProgress(updated_level, new_current_step, new_points )
                 }
             }
             Finished(final_points) => Finished(*final_points)
         }
+    }
+}
+
+#[cfg(test)]
+mod game_state_tests {
+    use super::*;
+    use crate::common::*;
+
+    static TEST_STR: &str =
+           "........\n\
+            2...@.##\n\
+            #4..3#..\n\
+            8..###..";
+
+    #[test]
+    fn next_state_too_many_moves_test() {
+        setup();
+
+        let max_steps = 15;
+
+        let test_level = Level::from_string(TEST_STR, max_steps);
+
+        let mut exhausted_current_game_state = GameState::from_initial_level(test_level.clone());
+        for _ in 0..(max_steps + 1) {
+            exhausted_current_game_state = exhausted_current_game_state.next_state(GameAction::MoveUp)
+        }
+
+        assert_eq!(exhausted_current_game_state, GameState::Finished(0));
+    }
+
+    #[test]
+    fn next_state_win_test() {
+        setup();
+
+        let max_steps = 15;
+
+        let test_level = Level::from_string(TEST_STR, max_steps);
+
+        //4 * left, right, down, down, left, right, up, right, right, right
+        let won_current_game_state =
+            GameState::from_initial_level(test_level.clone())
+                .next_state(GameAction::MoveLeft)
+                .next_state(GameAction::MoveLeft)
+                .next_state(GameAction::MoveLeft)
+                .next_state(GameAction::MoveLeft)
+                .next_state(GameAction::MoveRight)
+                .next_state(GameAction::MoveDown)
+                .next_state(GameAction::MoveDown)
+                .next_state(GameAction::MoveLeft)
+                .next_state(GameAction::MoveRight)
+                .next_state(GameAction::MoveUp)
+                .next_state(GameAction::MoveRight)
+                .next_state(GameAction::MoveRight)
+                .next_state(GameAction::MoveRight);
+
+        assert_eq!(won_current_game_state, GameState::Finished(19));
     }
 }
